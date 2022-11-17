fa3bb477b4afc6c39fe87a119e6e7caf656714b2
sqais <sqais379@mtroyal.ca>
2022-11-16 21:46:56 -0700

    Sort by year and popularity not working

--- js/assign2.js
+++ js/assign2.js
@@ -11,3 +11,60 @@ const api = 'http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.p
    Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
    use built-in Live Preview.
 */
+
+
+//list out artist names
+const art = JSON.parse(artists);
+for (let a of art) {
+   console.log(a.name);
+}
+console.log('---------------------------------------------------------------');
+
+//list out genres
+const gen = JSON.parse(genres);
+for (let g of gen) {
+   console.log(g.name);
+}
+console.log('---------------------------------------------------------------');
+
+//list out song list 
+const samp = JSON.parse(sampSongs);
+
+//sort by title
+console.log(samp.sort((a, b) => a.title.localeCompare(b.title)));
+console.log('....................');
+
+//sort by artist name 
+console.log(samp.sort((a, b) => a.artist.name.localeCompare(b.artist.name)));
+console.log('....................');
+
+//sort by year 
+console.log(samp.sort((a, b) => a.year - b.year));
+samp.sort(function(a,b){
+   return a.year - b.year;
+});
+console.log(samp);
+//console.log(samp.sort((a, b) =>  { return a.year-b.year;}));
+console.log('....................');
+
+//sort by genre
+console.log(samp.sort((a, b) => a.genre.name.localeCompare(b.genre.name)));
+console.log('....................');
+
+//sort by popularity
+console.log(samp.sort((a, b) => a.title.localeCompare(b.title)));
+console.log('....................');
+
+
+/*
+for (let ss of samp) {
+   console.log(ss.title);
+   console.log(ss.artist.name);
+   console.log(ss.year);
+   console.log(ss.genre.name);
+   console.log(ss.details.popularity);
+   console.log('....................')
+}
+*/
+
+
